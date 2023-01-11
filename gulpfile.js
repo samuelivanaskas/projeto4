09bd5ab178f6cf3fb2ba0b1456771120aa87640c
samuelivanaskas <106975862+samuelivanaskas@users.noreply.github.com>
2023-01-11 18:04:43 -0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const sass = require('gulp-sass')(require('sass'));
+const image = require('gulp-imagemin');
+
+function styles () {
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./dist/css'));
+}
+
+function images () {
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+}
